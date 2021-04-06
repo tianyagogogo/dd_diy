1f2eec89975571d4af4f5f4644a837f78db769f6
l107868382 <107868382@qq.com>
2021-04-06 16:58:38 +0800

    Update remote_crontab_list.sh

--- remote_crontab_list.sh
+++ remote_crontab_list.sh
@@ -1,9 +1,6 @@
 # 惊喜财富岛体现
 0 0 * * * node conc /scripts/l_jx_cfdtx.js |ts >> /scripts/logs/l_jx_cfdtx.log 2>&1
 
-# 定义京喜工厂运行时间
-50 6-24 * * * node /scripts/jd_dreamFactory.js |ts >> /scripts/logs/jd_dreamFactory.log 2>&1
-
 # 京东超市兑换
 59 23 * * * sleep 59; node conc /scripts/l_jd_bluecoin.js |ts >> /scripts/logs/l_jd_bluecoin.log 2>&1
 
