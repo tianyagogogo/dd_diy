acc339b3eb3210686a9526af98c97bbb7bea24f1
l107868382 <107868382@qq.com>
2021-04-06 10:14:38 +0800

    Update remote_crontab_list.sh

--- remote_crontab_list.sh
+++ remote_crontab_list.sh
@@ -7,3 +7,6 @@
 # monk_inter_shop_sign
 0 0 * * * node /scripts/monk_inter_shop_sign.js |ts >> /scripts/logs/monk_inter_shop_sign.log 2>&1
 
+#母婴-跳一跳
+5 8,14,20 6-13 4 * node /scripts/z_mother_jump.js |ts >> /scripts/logs/z_mother_jump.log 2>&1
+
