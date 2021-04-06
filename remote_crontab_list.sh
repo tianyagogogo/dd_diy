d996291c734c1c20ea3f2b1783115ded8552ae53
l107868382 <107868382@qq.com>
2021-04-06 09:27:31 +0800

    Update remote_crontab_list.sh

--- remote_crontab_list.sh
+++ remote_crontab_list.sh
@@ -4,3 +4,6 @@
 # 京东超市兑换
 59 23 * * * sleep 59; node conc /scripts/l_jd_bluecoin.js |ts >> /scripts/logs/l_jd_bluecoin.log 2>&1
 
+# monk_inter_shop_sign
+0 0 * * * node /scripts/monk_inter_shop_sign.js |ts >> /scripts/logs/monk_inter_shop_sign.log 2>&1
+
