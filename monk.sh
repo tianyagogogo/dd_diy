d3cb7ac6298a613b412d6af1d94f6aa0fe47868f
l107868382 <107868382@qq.com>
2021-04-13 17:33:35 +0800

    Create monk.sh

---
+++ monk.sh
@@ -0,0 +1,15 @@
+wget -P /dust  https://share.r2ray.com/dust/car/monk_shop_add_to_car.js
+wget -P /dust  https://share.r2ray.com/dust/car/monk_skyworth_car.js
+wget -P /dust  https://share.r2ray.com/dust/i-chenzhe/z_entertainment.js
+wget -P /dust  https://share.r2ray.com/dust/i-chenzhe/z_fanslove.js
+wget -P /dust  https://share.r2ray.com/dust/i-chenzhe/z_getFanslove.js
+wget -P /dust  https://share.r2ray.com/dust/i-chenzhe/z_marketLottery.js
+wget -P /dust  https://share.r2ray.com/dust/i-chenzhe/z_mother_jump.js
+wget -P /dust  https://share.r2ray.com/dust/i-chenzhe/z_shake.js
+wget -P /dust  https://share.r2ray.com/dust/i-chenzhe/z_shake.js
+wget -P /dust  https://share.r2ray.com/dust/i-chenzhe/z_super5g.js
+wget -P /dust  https://share.r2ray.com/dust/i-chenzhe/z_xmf.js /dust
+wget -P /dust  https://share.r2ray.com/dust/normal/monk_inter_shop_sign.js
+wget -P /dust  https://share.r2ray.com/dust/normal/monk_shop_follow_sku.js
+wget -P /dust  https://share.r2ray.com/dust/normal/monk_shop_lottery.js
+wget -P /dust  https://share.r2ray.com/dust/normal/monk_skyworth.js
