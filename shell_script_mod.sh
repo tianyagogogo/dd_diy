a22f4b178cf74bfa5a10fc0ca2a66b600d371421
l107868382 <107868382@qq.com>
2021-04-13 08:39:21 +0800

    Update shell_script_mod.sh

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -12,18 +12,18 @@ fi
 cp -f /jd_diy/scripts/*.js /scripts
 
 ## 克隆monk-coder仓库
-#if [ ! -d "/monk/" ]; then
-#    echo "未检查到monk-coder仓库脚本，初始化下载相关脚本..."
-#    git clone -b dust https://github.com/jonrea7/dust.git /monk
-#else
-#    echo "更新monk-coder脚本相关文件..."
-#    git -C /monk reset --hard
-#    git -C /monk pull origin dust --rebase
-#fi
-#cp -f /monk/car/*_*.js /scripts
-#cp -f /monk/i-chenzhe/*_*.js /scripts
-#cp -f /monk/member/*_*.js /scripts
-#cp -f /monk/normal/*_*.js /scripts
+if [ ! -d "/monk/" ]; then
+    echo "未检查到monk-coder仓库脚本，初始化下载相关脚本..."
+    git clone -b monk https://github.com/l107868382/dd_syc.git /monk
+else
+    echo "更新monk-coder脚本相关文件..."
+    git -C /monk reset --hard
+    git -C /monk pull origin monk --rebase
+fi
+cp -f /monk/car/*_*.js /scripts
+cp -f /monk/i-chenzhe/*_*.js /scripts
+cp -f /monk/member/*_*.js /scripts
+cp -f /monk/normal/*_*.js /scripts
 
 ## 拷贝京东超市兑换脚本
 cp -f /scripts/jd_blueCoin.js /scripts/l_jd_bluecoin.js
