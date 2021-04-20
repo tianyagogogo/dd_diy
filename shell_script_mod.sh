fb85f3525f7dba376ce27e29156c58f9fb9e822d
l107868382 <107868382@qq.com>
2021-04-20 11:09:11 +0800

    Update shell_script_mod.sh

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -25,6 +25,23 @@ cp -f /jd_diy/scripts/*.js /scripts
 #cp -f /monk/member/*_*.js /scripts
 #cp -f /monk/normal/*_*.js /scripts
 
+function diycron(){
+    # monkcoder whyour 定时任务
+    for jsname in /scripts/dust_*.js /scripts/whyour_*.js; /scripts/z_*.js; /scripts/monk_*.js; do
+        jsnamecron="$(cat $jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
+        test -z "$jsnamecron" || echo "$jsnamecron node $jsname >> /scripts/logs/$(echo $jsname | cut -d/ -f3).log 2>&1" >> /scripts/docker/merged_list_file.sh
+    done
+    # 启用京价保
+    # echo "23 8 * * * node /scripts/jd_price.js >> /scripts/logs/jd_price.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    # 修改docker_entrypoint.sh执行频率
+    #ln -sf /usr/local/bin/docker_entrypoint.sh /usr/local/bin/docker_entrypoint_mix.sh
+    #echo "35 */3 * * * docker_entrypoint_mix.sh >> /scripts/logs/default_task.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    # 整点京豆雨
+    echo "1 0-23/1 * * * node /scripts/jd_super_redrain.js >> /scripts/logs/jd_super_redrain.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    # 京喜财富岛提现
+    echo "59 23 * * * sleep 59; node /scripts/l_jx_cfdtx.js |ts >> /scripts/logs/l_jx_cfdtx.log 2>&1" >> /scripts/docker/merged_list_file.sh
+}
+
 ## 拷贝京东超市兑换脚本
 #cp -f /scripts/jd_blueCoin.js /scripts/l_jd_bluecoin.js
-cat /jd_diy/remote_crontab_list.sh >> /scripts/docker/merged_list_file.sh
+# cat /jd_diy/remote_crontab_list.sh >> /scripts/docker/merged_list_file.sh
