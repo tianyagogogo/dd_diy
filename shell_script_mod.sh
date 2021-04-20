12bba3528e92404418db0f27d4faefb41f5727be
l107868382 <107868382@qq.com>
2021-04-20 11:11:46 +0800

    Update shell_script_mod.sh

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-## 克隆jd_diy仓库
-if [ ! -d "/jd_diy/" ]; then
-    echo "未检查到克隆jd_diy仓库，初始化下载相关脚本..."
-    git clone -b lxk0301_shell https://github.com/l107868382/dd_diy.git /jd_diy
-else
-    echo "更新jd_diy脚本相关文件..."
-    git -C /jd_diy reset --hard
-    git -C /jd_diy pull origin lxk0301_shell --rebase
-fi
-cp -f /jd_diy/scripts/*.js /scripts
-
 ## 克隆monk-coder仓库
 #if [ ! -d "/monk/" ]; then
 #    echo "未检查到monk-coder仓库脚本，初始化下载相关脚本..."
@@ -42,7 +31,25 @@ function diycron(){
     echo "59 23 * * * sleep 59; node /scripts/l_jx_cfdtx.js |ts >> /scripts/logs/l_jx_cfdtx.log 2>&1" >> /scripts/docker/merged_list_file.sh
 }
 
-diycron
+function jd_diy(){
+    ## 克隆jd_diy仓库
+    if [ ! -d "/jd_diy/" ]; then
+        echo "未检查到克隆jd_diy仓库，初始化下载相关脚本..."
+        git clone -b lxk0301_shell https://github.com/l107868382/dd_diy.git /jd_diy
+    else
+        echo "更新jd_diy脚本相关文件..."
+        git -C /jd_diy reset --hard
+        git -C /jd_diy pull origin lxk0301_shell --rebase
+    fi
+    cp -f /jd_diy/scripts/*.js /scripts
+    
+}
+function main(){
+    jd_diy
+    diycron
+}
+
+
 
 ## 拷贝京东超市兑换脚本
 #cp -f /scripts/jd_blueCoin.js /scripts/l_jd_bluecoin.js
