d0d529dd41b952c5e3d3a8fb42ae9ee4dc1f3d30
l107868382 <107868382@qq.com>
2021-04-26 13:39:00 +0800

    Update shell_script_mod.sh

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -67,6 +67,9 @@ function jddj_diy(){
     #京东到家果园任务脚本
     echo "10 8,11,17 * * * node /scripts/jddj_fruit.js >> /scripts/logs/jddj_fruit.log 2>&1" >> /scripts/docker/merged_list_file.sh
     
+    # 京东到家果园水车收水滴任务脚本
+    echo "15 8,11,17 * * * node /scripts/jddj_fruit_collectWater.js >> /scripts/logs/jddj_fruit_collectWater.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    
     #京东到家鲜豆庄园收水滴脚本
     echo "*/20 * * * * node /scripts/jddj_getPoints.js >> /scripts/logs/jddj_getPoints.log 2>&1" >> /scripts/docker/merged_list_file.sh
     
