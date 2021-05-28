212c3628310de52a52496bb47f81e3dfa9aa3f91
l107868382 <107868382@qq.com>
2021-05-29 07:31:45 +0800

    Update shell_script_mod.sh
    123

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -128,7 +128,7 @@ function otherreplace(){
     
     # 注释掉 lxk jd_xtg的启动时间,新建启动时间
     sed -i "s/jd_xtg.js/jd_xtg_bak.js/g" /scripts/docker/merged_list_file.sh
-    echo "50 5 * * * node /scripts/jd_xtg.js >> /scripts/logs/jd_xtg.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    echo "0 6 * * * node /scripts/jd_xtg.js >> /scripts/logs/jd_xtg.log 2>&1" >> /scripts/docker/merged_list_file.sh
     
     sed -i "s/jd_xtg_help.js/jd_xtg_help_bak.js/g" /scripts/docker/merged_list_file.sh
     echo "0 6 * * * node /scripts/jd_xtg_help.js >> /scripts/logs/jd_xtg_help.log 2>&1" >> /scripts/docker/merged_list_file.sh
