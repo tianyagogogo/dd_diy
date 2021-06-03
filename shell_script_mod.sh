302413bb7fa2ea7f4a6da4e3296a82c79e8e3f33
l107868382 <107868382@qq.com>
2021-06-03 08:11:43 +0800

    Update shell_script_mod.sh
    # 京东星店长

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -111,10 +111,10 @@ function yangtingxiao_diy(){
         git -C /yangtingxiao reset --hard
         git -C /yangtingxiao pull origin master --rebase
     fi
-    cp -f /yangtingxiao/scripts/jd/jd_zoo.js /scripts
+    cp -f /yangtingxiao/scripts/jd/jd_starStore.js /scripts
     
-    # 动物联萌 618活动
-    echo "5 * * * * node /scripts/jd_zoo.js >> /scripts/logs/jd_zoo.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    # 京东星店长
+    echo "5 9 * * * node /scripts/jd_starStore.js >> /scripts/logs/jd_starStore.log 2>&1" >> /scripts/docker/merged_list_file.sh
 }
 
 
