c5e45ecbb62e93e861d7911e85b331d76c4c2d48
l107868382 <107868382@qq.com>
2021-06-07 18:17:00 +0800

    Update shell_script_mod.sh

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -81,15 +81,15 @@ function jddj_diy(){
     curl icanhazip.com > ./ipstr.txt
     iptxt=$(tail -1 ./ipstr.txt)
     ipbd="43.129"
-    if [[ "$iptxt" == "*$ipbd*" ]]
-    then
-      echo "l107868382服务器，复制个性化代码"
+    #if [[ "$iptxt" == "*$ipbd*" ]]
+    #then
+      #echo "l107868382服务器，复制个性化代码"
       rm -rf /jddj_diy/sendNotify.js
       cp -f /jddj_diy/*.js /scripts
       cp -f /scripts/logs/jddj_cookie.js /scripts
-    else
-      echo "非l107868382服务器，不复制个性化代码"
-    fi
+    #else
+      #echo "非l107868382服务器，不复制个性化代码"
+    #fi
 
     
 }
