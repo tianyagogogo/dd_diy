b410a4e289a347beb878a0bed277947883c89e4e
l107868382 <107868382@qq.com>
2021-06-10 11:10:51 +0800

    Update shell_script_mod.sh
    jd_zoo

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -87,10 +87,10 @@ function yangtingxiao_diy(){
         git -C /yangtingxiao reset --hard
         git -C /yangtingxiao pull origin master --rebase
     fi
-    cp -f /yangtingxiao/scripts/jd/jd_zoo.js.js /scripts
+    cp -f /yangtingxiao/scripts/jd/jd_zoo.js /scripts
     
     # 动物联萌 618活动
-    echo "5 * * * * node /scripts/jd_zoo.js.js >> /scripts/logs/jd_zoo.js.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    echo "5 * * * * node /scripts/jd_zoo.js >> /scripts/logs/jd_zoo.js.log 2>&1" >> /scripts/docker/merged_list_file.sh
 }
 
 
