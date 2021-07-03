d84e36599149e3d4fda11d59c86d2e6c87bb332d
l107868382 <107868382@qq.com>
2021-07-03 18:57:04 +0800

    tear

--- shell/auto_help.sh
+++ shell/auto_help.sh
@@ -61,6 +61,7 @@ exportSharecode() {
     #判断合成的助力码长度是否大于账号数，不大于，则可知没有助力码
     if [ ${#allSharecode} -gt ${cookiecount} ]; then
       echo "${1}：导出助力码"
+      echo "导出的助力码为： ${allSharecode}"
       export ${3}=${allSharecode}
     else
       echo "${1}：没有助力码，不导出"
