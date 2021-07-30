3d6eabac41fabc4b60d351ae122171422bd90f33
l107868382 <107868382@qq.com>
2021-07-30 09:05:22 +0800

    Update jd-bash.sh

--- shell/jd-bash.sh
+++ shell/jd-bash.sh
@@ -20,7 +20,7 @@ function diycron(){
     echo "10 7 1-31/7 * * node /scripts/jd_all_bean_change.js >> /scripts/logs/jd_all_bean_change.log 2>&1" >> /scripts/docker/merged_list_file.sh
     
     # 领现金兑换红包
-    echo "59 23 * * 4,5 sleep 50; node /scripts/jd_cash_exchange.js >> /scripts/logs/jd_cash_exchange.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    echo "59 23 * * 4,5 sleep 57; node /scripts/jd_cash_exchange.js >> /scripts/logs/jd_cash_exchange.log 2>&1" >> /scripts/docker/merged_list_file.sh
     
     
 }
