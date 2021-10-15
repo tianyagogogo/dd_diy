6152837fad0abb7fa53001c90713126b22f6cf62
l107868382 <107868382@qq.com>
2021-10-15 09:35:49 +0800

    Create jd-bash.sh

---
+++ shell/jd-bash.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+function diycron(){
+
+  # 修改docker_entrypoint.sh执行频率
+  ln -sf /usr/local/bin/docker_entrypoint.sh /usr/local/bin/docker_entrypoint_mix.sh
+  echo "18 */1 * * * docker_entrypoint_mix.sh >> /scripts/logs/default_task.log 2>&1" >> /scripts/docker/merged_list_file.sh
+  
+  
+  
+   for jsname in /scripts/jddj_*.js ; do
+        jsnamecron="$(cat $jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
+        test -z "$jsnamecron" || echo "$jsnamecron node $jsname >> /scripts/logs/$(echo $jsname | cut -d/ -f3).log 2>&1" >> /scripts/docker/merged_list_file.sh
+   done
+  
+  # 领现金兑换红包
+  echo "59 23 * * 4,5 sleep 59.7; node conc /scripts/jd_cash_exchange.js >> /scripts/logs/jd_cash_exchange.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    
+}
+
+
+function jd_diy(){
+    cp -f /jd_diy/scripts/*.js /scripts
+    cat /dev/null > /scripts/docker/auto_help.sh
+    cat /jd_diy/shell/auto_help.sh >> /scripts/docker/auto_help.sh
+}
+
+
+
+function jddj_diy(){
+    ## 克隆jddj_diy仓库
+    if [ ! -d "/jddj_diy/" ]; then
+        echo "未检查到克隆jddj_diy仓库，初始化下载相关脚本..."
+        git clone -b main https://ghproxy.com/https://github.com/passerby-b/JDDJ.git /jddj_diy
+    else
+        echo "更新jddj_diy脚本相关文件..."
+        git -C /jddj_diy reset --hard
+        git -C /jddj_diy pull origin main --rebase
+    fi  
+    cp -f /jddj_diy/jddj_*.js /scripts
+}
+
+
+
+
+
+
+
+
+# 替换
+function otherreplace(){
+    echo " otherreplace "    
+    sed -i "s/https:\/\/wuzhi03.coding.net\/p\/dj\/d\/RandomShareCode\/git\/raw\/main\/JD_Fruit.json/https:\/\/ghproxy.com\/https:\/\/raw.githubusercontent.com\/l107868382\/sharcode\/main\/v1\/JD_Fruit.json/g" /scripts/jd_fruit.js
+    sed -i "s/let helpAuthor = true/let helpAuthor = false/g" /scripts/jd_fruit.js
+    sed -i "s/https:\/\/wuzhi03.coding.net\/p\/dj\/d\/RandomShareCode\/git\/raw\/main\/JD_Cash.json/https:\/\/ghproxy.com\/https:\/\/raw.githubusercontent.com\/l107868382\/sharcode\/main\/v1\/JD_Cash.json/g" /scripts/jd_cash.js
+    sed -i "s/https:\/\/wuzhi03.coding.net\/p\/dj\/d\/shareCodes\/git\/raw\/main\/jd_updateCash.json/https:\/\/ghproxy.com\/https:\/\/raw.githubusercontent.com\/l107868382\/sharcode\/main\/v1\/jd_updateCash.json/g" /scripts/jd_cash.js
+    sed -i "s/https:\/\/wuzhi03.coding.net\/p\/dj\/d\/shareCodes\/git\/raw\/main\/jd_red.json/https:\/\/ghproxy.com\/https:\/\/raw.githubusercontent.com\/l107868382\/sharcode\/main\/v1\/jd_red.json/g" /scripts/jd_redPacket.js
+    
+    #惊喜88红包
+    #sed -i "s/https:\/\/wuzhi03.coding.net\/p\/dj\/d\/shareCodes\/git\/raw\/main\/jd_redhb.json/https:\/\/ghproxy.com\/https:\/\/raw.githubusercontent.com\/l107868382\/sharcode\/main\/v1\/jd_redhb.json/g" /scripts/jd_jxlhb.js
+    
+    
+    
+    #京豆变化
+    sed -ie '/jd_bean_change.js/d' /scripts/docker/merged_list_file.sh
+    echo "0 9,20 * * * node /scripts/jd_bean_change.js >> /scripts/logs/jd_bean_change.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    
+  
+    
+}
+
+
+
+
+
+function main(){
+    jddj_diy
+    jd_diy
+    diycron
+    otherreplace
+}
+
+main
