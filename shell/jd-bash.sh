a61425b2b416a45f4cf8046e1b0e344ef2a07f89
l107868382 <107868382@qq.com>
2021-10-20 15:26:22 +0800

    Update jd-bash.sh

--- shell/jd-bash.sh
+++ shell/jd-bash.sh
@@ -107,13 +107,12 @@ function otherreplace(){
     echo "0 9,20 * * * node /scripts/jd_bean_change.js >> /scripts/logs/jd_bean_change.log 2>&1" >> /scripts/docker/merged_list_file.sh
     
     #东东工厂
-     sed -ie '/jd_jdfactory.js/d' /scripts/docker/merged_list_file.sh
-     echo "26 * * * * node /scripts/jd_jdfactory.js >> /scripts/logs/jd_jdfactory.log 2>&1" >> /scripts/docker/merged_list_file.sh
+     #sed -ie '/jd_jdfactory.js/d' /scripts/docker/merged_list_file.sh
+     #echo "26 * * * * node /scripts/jd_jdfactory.js >> /scripts/logs/jd_jdfactory.log 2>&1" >> /scripts/docker/merged_list_file.sh
   
-    #jd_cfd_loop.js
-    #sed -ie '/jd_cfd_loop.js/d' /scripts/docker/merged_list_file.sh
-    #echo "26 1,13 * * * node /scripts/jd_cfd_loop.js >> /scripts/logs/jd_cfd_loop.log 2>&1" >> /scripts/docker/merged_list_file.sh
-    
+    # 删除开卡任务
+    sed -ie '/jd_opencard/d' /scripts/docker/merged_list_file.sh
+
 }
 
 
