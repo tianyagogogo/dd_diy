545fe8f55261547eec7a5477a24148c4254fc021
l107868382 <107868382@qq.com>
2021-10-21 08:35:49 +0800

    Update jd-bash.sh

--- shell/jd-bash.sh
+++ shell/jd-bash.sh
@@ -19,7 +19,7 @@ function diycron(){
   # 京东月资产变动通知
   echo "10 7 1-31/7 * * node /scripts/jd_all_bean_change.js >> /scripts/logs/jd_all_bean_change.log 2>&1" >> /scripts/docker/merged_list_file.sh
   # 领现金兑换红包
-  echo "59 23 * * 4,5 sleep 59.7; node conc /scripts/jd_cash_exchange.js >> /scripts/logs/jd_cash_exchange.log 2>&1" >> /scripts/docker/merged_list_file.sh
+  # echo "59 23 * * 4,5 sleep 59.7; node conc /scripts/jd_cash_exchange.js >> /scripts/logs/jd_cash_exchange.log 2>&1" >> /scripts/docker/merged_list_file.sh
     
 }
 
@@ -112,6 +112,10 @@ function otherreplace(){
   
     # 删除开卡任务
     sed -ie '/jd_opencard/d' /scripts/docker/merged_list_file.sh
+    
+    # 城城分现金
+    sed -ie '/jd_CityNew.js/d' /scripts/docker/merged_list_file.sh
+    echo "0 0 * * * node conc /scripts/jd_CityNew.js >> /scripts/logs/jd_CityNew.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
 }
 
