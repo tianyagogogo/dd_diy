ee80ec6e99bed96b6352aba98b4eeaad36fc9613
l107868382 <107868382@qq.com>
2021-11-17 08:57:57 +0800

    Update jd-bash.sh

--- shell/jd-bash.sh
+++ shell/jd-bash.sh
@@ -132,7 +132,7 @@ function otherreplace(){
     # 京东极速版红包
     sed -ie '/jd_speed_redpocke/d' /scripts/docker/merged_list_file.sh
     # 发财挖宝
-    sed -ie '/jd_fcwb.js/d' /scripts/docker/merged_list_file.sh
+    sed -ie '/jd_fcwb/d' /scripts/docker/merged_list_file.sh
     # 省钱大赢家翻翻乐
     sed -ie '/jd_big_winner/d' /scripts/docker/merged_list_file.sh
     # 推一推
