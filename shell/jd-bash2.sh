9b0b35ceb8e60105a58f5cb913b9ed95af81d251
l107868382 <107868382@qq.com>
2021-11-18 10:36:28 +0800

    Update jd-bash2.sh

--- shell/jd-bash2.sh
+++ shell/jd-bash2.sh
@@ -10,9 +10,13 @@ function jd_diy(){
 function main(){
    jd_diy
    cat /dev/null > /scripts/docker/merged_list_file.sh
-   #echo "25 15 * * * node /scripts/jd_speed_sign.js >> /scripts/logs/jd_speed_sign.log 2>&1" >> /scripts/docker/merged_list_file.sh
    
+   # 极速版签到
    echo "25 12 * * * sh +x /scripts/logs/synck.sh node /scripts/jd_speed_sign.js >> /scripts/logs/jd_speed_sign.log 2>&1" >> /scripts/docker/merged_list_file.sh
+   
+   # 极速版红包
+   echo "25 6 * * * sh +x /scripts/logs/synck.sh node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1" >> /scripts/docker/merged_list_file.sh
+   
 }
 
 main
