e0f5e4c657b38cb16850befc8b979f7edfc43541
l107868382 <107868382@qq.com>
2021-11-22 10:22:24 +0800

    Update jd-bash.sh

--- shell/jd-bash.sh
+++ shell/jd-bash.sh
@@ -123,6 +123,10 @@ function otherreplace(){
     #sed -ie '/jd_fanli/d' /scripts/docker/merged_list_file.sh
     #echo "0 0,18 * * * node /scripts/jd_fanli.js >> /scripts/logs/jd_fanli.log 2>&1" >> /scripts/docker/merged_list_file.sh
     
+     # 发财挖宝
+     sed -ie '/jd_fcwb/d' /scripts/docker/merged_list_file.sh
+     # echo "5 7 * * * node /scripts/jd_fcwb.js >> /scripts/logs/jd_fcwb.log 2>&1" >> /scripts/docker/merged_list_file.sh
+     
     #------------------------------黑号删除脚本-----------------------------------------
     # 京东极速版红包
     sed -ie '/jd_speed_redpocke/d' /scripts/docker/merged_list_file.sh
@@ -139,10 +143,9 @@ function otherreplace(){
     # 极速版签到
     sed -ie '/jd_speed_sign/d' /scripts/docker/merged_list_file.sh
     echo "11 6 */7 * * node /scripts/jd_speed_sign.js >> /scripts/logs/jd_speed_sign.log 2>&1" >> /scripts/docker/merged_list_file.sh
-     # 发财挖宝
-     sed -ie '/jd_fcwb/d' /scripts/docker/merged_list_file.sh
-     echo "5 7 * * * node /scripts/jd_fcwb.js >> /scripts/logs/jd_fcwb.log 2>&1" >> /scripts/docker/merged_list_file.sh
+   
    fi
+   
    
 }
 
