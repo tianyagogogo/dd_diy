5737762451a86730e85e3c8fbd74f83e0acc1dda
l107868382 <107868382@qq.com>
2021-12-15 16:57:09 +0800

    Update diybase.sh

--- shell/diybase.sh
+++ shell/diybase.sh
@@ -5,8 +5,13 @@ function jd_diy(){
     cp -f /jd_diy/scripts/* /ql/scripts/shufflewzc_faker2
 }
 
+function replace(){
+    sed -i "s/http:\/\/transfer.nz.lu\/farm/https:\/\/ghproxy.com\/https:\/\/github.com\/l107868382\/sharcode\/blob\/main\/v1\/JD_Fruit.json/g" /ql/scripts/shufflewzc_faker2/jd_fruit.js
+}
+
 function main(){
     jd_diy
+    replace
 }
 
 main
