70ec0ea675a6cf44a0e5e216996f26df4d2a1d2e
l107868382 <107868382@qq.com>
2021-12-27 15:01:23 +0800

    Update diybase.sh

--- shell/diybase.sh
+++ shell/diybase.sh
@@ -7,6 +7,10 @@ function jd_diy(){
 
 function replace(){
     sed -i "s/http:\/\/transfer.nz.lu\/farm/https:\/\/ghproxy.com\/https:\/\/github.com\/l107868382\/sharcode\/blob\/main\/v1\/JD_Fruit.json/g" /ql/scripts/shufflewzc_faker2/jd_fruit.js
+    sed -i "s/https:\/\/raw.githubusercontent.com\/Aaron-lv\/updateTeam\/master\/shareCodes\/cfd.json/https:\/\/raw.githubusercontent.com\/l107868382\/sharcode\/main\/v1\/jxcfd.json/g" /ql/scripts/shufflewzc_faker2/jd_cfd.js
+    sed -i "s/https:\/\/purge.jsdelivr.net\/gh\/Aaron-lv\/updateTeam@master\/shareCodes\/cfd.json/https:\/\/raw.githubusercontent.com\/l107868382\/sharcode\/main\/v1\/jxcfd.json/g" /ql/scripts/shufflewzc_faker2/jd_cfd.js
+    sed -i "s/https:\/\/cdn.jsdelivr.net\/gh\/Aaron-lv\/updateTeam@master\/shareCodes\/cfd.json/https:\/\/raw.githubusercontent.com\/l107868382\/sharcode\/main\/v1\/jxcfd.json/g" /ql/scripts/shufflewzc_faker2/jd_cfd.js
+    sed -i "s/https:\/\/transfer.nz.lu\/cfd/https:\/\/raw.githubusercontent.com\/l107868382\/sharcode\/main\/v1\/jxcfd.json/g" /ql/scripts/shufflewzc_faker2/jd_cfd.js
 }
 
 function main(){
