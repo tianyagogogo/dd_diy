890584591396f1be46aa18b206b4a974f5ad193f
l107868382 <107868382@qq.com>
2022-01-20 13:41:03 +0800

    Update jd_redEnvelope.js

--- scripts/jd_redEnvelope.js
+++ scripts/jd_redEnvelope.js
@@ -141,7 +141,7 @@ let timeH = $.time('H')
 
 async function run(type = 0){
   try{
-    rebateCodes = [""];
+    rebateCodes = ["Stm9YoS"];
     rebateCodes = rebateCodes[Math.floor((Math.random() * rebateCodes.length))]
     rebateCodes = $.isNode() ? (process.env.gua_nhjRed_rebateCode ? process.env.gua_nhjRed_rebateCode : `${rebateCodes}`) : ($.getdata('gua_nhjRed_rebateCode') ? $.getdata('gua_nhjRed_rebateCode') : `${rebateCodes}`);
     rebateCode = rebateCodes
