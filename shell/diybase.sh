211c1d1e28d4e7603a5de716d3d9a48a27eff9f3
l107868382 <107868382@qq.com>
2022-02-04 19:51:55 +0800

    Update diybase.sh

--- shell/diybase.sh
+++ shell/diybase.sh
@@ -2,7 +2,9 @@
 
 function jd_diy(){
     cp -f /jd_diy/scripts/sendNotify.js /ql/scripts
-    cp -f /jd_diy/scripts/* /ql/scripts/shufflewzc_faker2
+    # cp -f /jd_diy/scripts/* /ql/scripts/shufflewzc_faker2
+    cp -f /jd_diy/scripts/*  /ql/scripts/Aaron-lv_sync_jd_scripts
+ 
 }
 
 function replace(){
